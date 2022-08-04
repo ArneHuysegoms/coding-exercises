69fa24783be7eb147ffbb076621004c57dcb0a7f
Arne <arhuy@hotmail.com>
2022-08-04 12:47:33 +0200

    Music application init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ColorService} from "./paint/color.service";
+import {ColorService} from "./paint/services/color.service";
 import { PaintComponent } from './paint/paint.component';
 import { MusicComponent } from './music/music.component';
 
